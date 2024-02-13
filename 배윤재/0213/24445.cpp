594fcd571b4243d651f647215576f2b34144350b
baeyj <qodbswo1010@gmail.com>
2024-02-14 00:37:19 +0900

    2024-02-13/1문제

---
+++ 배윤재/0213/24445.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+vector<int> alis[100001];
+int visited[100001];
+int cnt = 1;
+
+void bfs(int start)
+{
+	queue<int> q;
+
+	q.push(start);
+	visited[start] = cnt++;
+
+	while (!q.empty())
+	{
+		int now = q.front();
+		q.pop();
+
+		for (int i = 0; i < alis[now].size(); i++)
+		{
+			int next = alis[now][i];
+			if (visited[next] != 0) continue;
+
+			visited[next] = cnt++;
+			q.push(next);
+		}
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n, m, r;
+	cin >> n >> m >> r;
+
+	for (int i = 0; i < m; i++)
+	{
+		int from, to;
+		cin >> from >> to;
+		alis[from].push_back(to);
+		alis[to].push_back(from);
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		sort(alis[i].begin(), alis[i].end(), greater<>());
+	}
+
+
+	bfs(r);
+
+	for (int i = 1; i <= n; i++)
+	{
+		cout << visited[i] << "\n";
+	}
+
+
+	return 0;
+}
