956a578895b7732bbbc8ad6181faa3d1ec031ca7
suhyun0927 <dsw9909@gmail.com>
2024-04-03 16:34:44 +0900

    0403

---
+++ 강수현/G3/[0403] 2252. 줄 세우기.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+using namespace std;
+
+int N, M;
+vector<int>v[32001];
+int inDegree[32001];
+
+void bfs() {
+	priority_queue<int>pq;
+
+	// init
+	for (int i = 1; i <= N; i++) {
+		if (inDegree[i] != 0) continue;
+		pq.push(i);
+	}
+
+	while (!pq.empty()) {
+		int now = pq.top();
+		pq.pop();
+		cout << now << " ";
+
+		for (int i = 0; i < v[now].size(); i++) {
+			inDegree[v[now][i]]--;
+
+			if (inDegree[v[now][i]] == 0) pq.push(v[now][i]);
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N >> M;
+
+	for (int i = 0; i < M; i++) {
+		int a, b;
+		cin >> a >> b;
+		v[a].push_back(b);
+		inDegree[b]++;
+	}
+
+	bfs();
+
+	return 0;
+}
